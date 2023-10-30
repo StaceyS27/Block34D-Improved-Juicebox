3beae7190f488c5c109d38a1b0c33fd64f7669e3
Stacey Sanchez <staceysanchez27@gmail.com>
2023-10-30 15:30:57 -0400

    login endpoint created

--- server/auth/index.js
+++ server/auth/index.js
@@ -60,6 +60,34 @@ authRouter.post('/register', async (req, res, next) => {
 
 
 //POST /auth/login - login a previous user
+authRouter.post("/login", async(req, res, next) => {
+    try {
+        const password = req.body.password;
+
+        const user = await prisma.users.findUnique({
+            where: {
+               username: req.body.username, 
+            }
+        });
+
+        if(!user) {
+            return res.status(401).send("Invalid login credentials.")
+        };
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordsMatch) {
+            return res.status(401).send("Invalid login credentials.");
+        }
+
+        const token = jwt.sign({id: user.id}, process.env.JWT);
+
+        res.send("You have logged in! \n Token: " + token);
+
+    } catch (error) {
+        res.send("unable to log in.")
+    }
+})
 
 
 module.exports = authRouter
