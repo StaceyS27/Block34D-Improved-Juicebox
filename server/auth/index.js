a11361afc090e36f8198fbc7d1a002460c46d07b
Stacey Sanchez <staceysanchez27@gmail.com>
2023-10-29 22:46:56 -0400

    get all users

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const authRouter = express.Router();
 
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
+
 //test functionality of router
 authRouter.get('/', (req, res, next)=> {
     res.send("Auth Router!");
-})
+});
+
+//GET /auth/users - get all users
+authRouter.get('/users', async(req, res, next) => {
+    try{
+        const users = await prisma.users.findMany();
+        res.send(users)
+    } catch(error) {
+        res.send("unable to get all users")
+    }
+});
+
 
 
 module.exports = authRouter
